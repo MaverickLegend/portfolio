9899d86173c125ddd88b455707a2521699cf8333
Cristián Rojas <malcolmrojas.f@gmail.com>
2025-03-11 15:12:51 -0300

    fixing img errors

--- src/router/index.ts
+++ src/router/index.ts
@@ -3,8 +3,7 @@ import { createRouter, createWebHistory, RouteRecordRaw } from "vue-router";
 const routes: Array<RouteRecordRaw> = [
   {
     path: "/",
-    name: "About",
-    component: () => import("../components/AboutComponent.vue"),
+    redirect: "/about",
   },
   {
     path: "/about",
