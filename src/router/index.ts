0c744c17fe2805ea460fd59789a75915766c04d7
Cristián Rojas <malcolmrojas.f@gmail.com>
2025-05-11 23:44:09 -0400

    scrollBehavior fix

--- src/router/index.ts
+++ src/router/index.ts
@@ -30,7 +30,7 @@ const routes: Array<RouteRecordRaw> = [
 const router = createRouter({
   history: createWebHistory(import.meta.env.BASE_URL),
   routes,
-  scrollBehavior(to, from, savedPosition) {
+  scrollBehavior(_to, _from, savedPosition) {
     if (savedPosition) {
       return savedPosition;
     }
